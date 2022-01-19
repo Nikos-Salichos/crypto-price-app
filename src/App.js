23a06f2bc68bc70e3617fb713f7fbda77c074cc4
Nikos S <niksal1991@gmail.com>
2022-01-19 20:59:34 +0200

    Get Api Call with Axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React, { useEffect } from 'react';
+import Axios from 'axios';
 
 function App() {
+
+  useEffect(() => {
+    Axios.get('https://api.coinstats.app/public/v1/coins?skip=0&limit=10').then((response) => console.log(response))
+  }, [])
+
+
   return (
     <div className="App">
-      <h1></h1>
+      <div className='cryptoHeader'></div>
+      <div className='cryptoDisplay'></div>
     </div>
   );
 }
